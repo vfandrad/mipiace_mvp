737c214d69618209fca61b6cdea45025d03eac1a
Victor Andrade <victor.f.andrade@outlook.com>
2026-04-01 21:37:51 -0400

    full delete

--- front/src/lib/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-// Criamos uma instância do Axios configurada
-export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
